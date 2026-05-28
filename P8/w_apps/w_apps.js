cb027ffc42cc8dff2ee05069851b85bfef478e3c
bonefunk <jquadrel@gmail.com>
2026-05-28 10:21:38 -0400

    Update w_apps.js
    added sail as app 4

--- P8/w_apps/w_apps.js
+++ P8/w_apps/w_apps.js
@@ -35,12 +35,12 @@ if (global.tpms) {
 }else g.drawImage(this.appImgNone,176,15);
 g.flip();
 //app4
-//this.appDo4="face.go('hello',0)";
+this.appDo4="face.go('sail',0)";
 g.setColor(1,1);
 g.fillRect(0,80,75,155);
 g.setColor(0,15);
-if (require("Storage").read("hello",1)) {
-	this.appDo4="face.go('hello',0)";
+if (require("Storage").read("sail",1)) {
+	this.appDo4="face.go('sail',0)";
 	this.img=require("heatshrink").decompress(atob("mEwwIIFn4FKmYFHv/ggYFBgf//kD2cAh/An8B+cAngNB4AFBuAZB8F/gPgAoN4j0DwAFBjkMgYuCh0GgwFCAgMMAoUDg0cAoeGjEHwEegOGmEfFoOA4cwjFgsIFBsBwB4HAsYFL8AFDufADocz4ApDAoI1CPoPAQYgFEn4FKFgIFEPgQABuFgAodgApcx/wFC4AFFjCnCgOAjk4RIWAhhRBAoKdBg/AnilBg8B//4hwTCKAKlBYohJCaIQCBboYFBdIS8DeoJQDv4WCA"));
 	g.drawImage(this.img,13,94);
 }else g.drawImage(this.appImgNone,13,94);
